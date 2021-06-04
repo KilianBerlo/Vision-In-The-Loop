5dff6cc2fb4e6e9e6c93111ede79626d5b78cf9f
Glen <glen@tehofsteengineering.nl>
2021-06-04 10:53:17 +0200

    Updated quadrature encoder.

--- quadrature_encoder/quadrature_encoder.vhd
+++ quadrature_encoder/quadrature_encoder.vhd
@@ -14,50 +14,92 @@ end quadrature_encoder;
 
 architecture behaviour of quadrature_encoder is
 
-signal prev_input_state: std_logic_vector(1 DOWNTO 0) := "11";
-
 begin
 	
 	-- Signal decoder.
-	process (clk, n_rst, enc_in, prev_input_state)
-	
-	
+	process (clk, n_rst, enc_in)
+		
 
-	-- Direction type
-	type direction_type is (clockwise, counterclockwise, idle);
-	variable direction : direction_type := idle;
-	
-	
-
-	variable tick_counter : signed(31 downto 0)   := (others => '0');
+	variable counter : signed(31 downto 0) := (others => '0');
+	variable prev_input_state: std_logic_vector(1 DOWNTO 0) := enc_in;
 		
 	begin
 	
 			if (n_rst = '0') then
 			
-				tick_counter := (others => '0');
-				prev_input_state <= "11";
-				direction := idle;
+				counter := (others => '0');
+				prev_input_state := enc_in;
 			
 			-- Only enter this if statement if the input state has changed.
 			elsif rising_edge(clk) and enc_in /= prev_input_state then
+			
+				case prev_input_state is
 				
-				if enc_in = "01" and prev_input_state = "00" then 
+					when "11" => 
 					
-					tick_counter := tick_counter + 1;
-				
-				elsif enc_in = "10" and prev_input_state = "00" then 
+						if enc_in = "10" then 
+						
+							counter := counter - 1;
+						
+						end if;
+						
+						if enc_in = "01" then 
+						
+							counter := counter + 1;
+						
+						end if;
 					
-					tick_counter := tick_counter - 1;
+					when "01" => 
+					
+						if enc_in = "11" then 
+						
+							counter := counter - 1;
+						
+						end if;
+						
+						if enc_in = "00" then 
+						
+							counter := counter + 1;
+						
+						end if;
+					
+					when "10" =>
+					
+						if enc_in = "00" then 
+						
+							counter := counter - 1;
+						
+						end if;
+						
+						if enc_in = "11" then 
+						
+							counter := counter + 1;
+						
+						end if;
+					
+					when "00" =>
+					
+						if enc_in = "01" then 
+						
+							counter := counter - 1;
+						
+						end if;
+						
+						if enc_in = "10" then 
+						
+							counter := counter + 1;
+						
+						end if;
 				
-				end if;
+				end case;
+			
 				
-				prev_input_state <= enc_in;
+				prev_input_state := enc_in;
 							
 			end if;
 			
 			-- Route the counter to the output.
-			count <= std_logic_vector(tick_counter);
+			count <= std_logic_vector(counter);
 			
 	end process;
 
