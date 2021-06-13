865214a7b687e9cd7582b97c9a5ce98b052f4718
KilianBerlo <k.v.berlo@outlook.com>
2021-06-13 11:14:09 +0200

    Added some more comments for better understanding of the code

--- DisplayImage.cpp
+++ DisplayImage.cpp
@@ -1,24 +1,21 @@
-// Main header for GStreamer
-#include <gst/gst.h>
-// Used for several funcions of opencv for image processing
-#include "opencv2/imgproc.hpp"
-
-// For saving the images with imwrite for debugging 
-#include "opencv2/highgui.hpp"
-// For writing x and y values to a file for debugging
-#include <fstream>
+#include <gst/gst.h>	//Main header for GStreamer
+#include "opencv2/imgproc.hpp"	//Used for several funcions of opencv for image processing
+#include "opencv2/highgui.hpp"	//For saving the images with imwrite for debugging 
+#include <fstream>	//For writing x and y values to a file for debugging
 
 using namespace std;
 using namespace cv;
 
-int Hue_Low= 4;//lower range of hue//
-int Hue_high = 84;//upper range of hue//
-int Sat_Low =136;//lower range of saturation//
-int Sat_high = 255;//upper range of saturation//
-int Val_Low = 150;//lower range of value//
-int Val_high = 255;//upper range of value// 
-int Horizontal_Last = -1;//initial horizontal position//
-int vertical_Last = -1;//initial vertical position//
+int res_w = 320;	//Resolution width
+int res_h = 240;	//Resolution height
+int hue_low= 4;	//Lower range of hue
+int hue_high = 84;	//Upper range of hue
+int sat_low =136;	//Lower range of saturation
+int sat_high = 255;	//Upper range of saturation
+int val_low = 150;	//Lower range of value
+int val_high = 255;	//Upper range of value 
+int horizontal_last = -1;	//Horizontal position
+int vertical_last = -1;	//Vertical position
 
 typedef struct _AllElements 
 {
@@ -29,14 +26,17 @@ static gboolean bus_call (GstBus *bus, GstMessage *msg, gpointer data)
 {
   GMainLoop *loop = (GMainLoop *) data;
 
-  switch (GST_MESSAGE_TYPE (msg)) {
-
+  switch (GST_MESSAGE_TYPE (msg)) 
+  {
     case GST_MESSAGE_EOS:
+    {
       g_print ("End of stream\n");
       g_main_loop_quit (loop);
       break;
+    }
 
-    case GST_MESSAGE_ERROR: {
+    case GST_MESSAGE_ERROR: 
+    {
       gchar  *debug;
       GError *error;
 
@@ -49,10 +49,11 @@ static gboolean bus_call (GstBus *bus, GstMessage *msg, gpointer data)
       g_main_loop_quit (loop);
       break;
     }
+    
     default:
       break;
   }
-
+  
   return TRUE;
 }
 
@@ -71,49 +72,46 @@ static GstFlowReturn new_sample (GstElement *sink, AllElements *element)
      g_print("Check frame \n");
      if (sample) 
      {
-        buffer = gst_sample_get_buffer(sample);
+        buffer = gst_sample_get_buffer(sample);	//Get the buffer associated with sample
         if (gst_buffer_map(buffer, &map, GST_MAP_READ)) 
         {
             	g_print("size: %ld \n", map.size);
-            	Mat frameYUV(240+240/2, 320, CV_8UC1 , map.data, Mat::AUTO_STEP);
-            	Mat frameRGB(240, 320, CV_8UC4);
-            	cvtColor(frameYUV, frameRGB, COLOR_YUV2BGRA_I420);
+            	Mat frameYUV(res_h+res_h/2, res_w, CV_8UC1 , map.data, Mat::AUTO_STEP); //Converting mapping of sample to openCV Mat 
+            	Mat frameRGB(res_h, res_w, CV_8UC4);	//Creating a matrix for RGB values
+            	cvtColor(frameYUV, frameRGB, COLOR_YUV2BGRA_I420);	//Converting YUV image to RGB 
 	    	imwrite("checkRGB.jpg", frameRGB);
 	    	
-		Mat track_motion = Mat::zeros(frameRGB.size(), CV_8UC4);//creating black matrix for detection//
-		Mat converted_to_HSV;//declaring a matrix to store converted image//
-      		cvtColor(frameRGB, converted_to_HSV, COLOR_BGR2HSV);//converting BGR image to HSV//
+		Mat track_motion = Mat::zeros(frameRGB.size(), CV_8UC4);	//Creating a matrix with zeros (black) for detection
+		Mat converted_to_HSV;	//Declaring a matrix to store BGR to HSV converted image
+      		cvtColor(frameRGB, converted_to_HSV, COLOR_BGR2HSV);	//Converting BGR image to HSV
 		imwrite("checkHSV.jpg", converted_to_HSV);
 		
-      		Mat adjusted_frame;//declaring a matrix to detected color//
-      		inRange(converted_to_HSV, Scalar(Hue_Low, Sat_Low, Val_Low), Scalar(Hue_high, Sat_high, Val_high), adjusted_frame);//applying change of values of track-bars//      
+      		Mat adjusted_frame;	//Declaring a matrix for detected object color
+      		inRange(converted_to_HSV, Scalar(hue_low, sat_low, val_low), Scalar(hue_high, sat_high, val_high), adjusted_frame);	//Applying range of HSV values to detect  
 		imwrite("checkDetect.jpg", adjusted_frame);
 		
-		Moments detecting_object = moments(adjusted_frame);//creating an object from detected color frame//
-	      	double vertical_moment = detecting_object.m01;//getting value of vertical position//
-	      	double horizontal_moment = detecting_object.m10;//getting value of horizontal position//
-	      	double tracking_area = detecting_object.m00;//getting area of the object//
-		if (tracking_area > 10000){ //when area of the object is greater than 10000 pixels//
-			 int posX = horizontal_moment / tracking_area;//calculate the horizontal position of the object//
-			 int posY = vertical_moment / tracking_area;//calculate the vertical position of the object//
-			 Horizontal_Last = posX;//getting new horizontal position//
-			 vertical_Last = posY;// getting new vertical position value//
+		Moments detecting_object = moments(adjusted_frame);	//Creating an object from the detected color frame
+	      	double vertical_moment = detecting_object.m01;	//Getting value of vertical position of the object
+	      	double horizontal_moment = detecting_object.m10;	//Getting value of horizontal position of the object
+	      	double tracking_area = detecting_object.m00;	//Getting area of the object
+		if (tracking_area > 10000){	//When the area of the object is greater than 10000 pixels
+			 int posX = horizontal_moment / tracking_area;	//Calculate the horizontal position of the object
+			 int posY = vertical_moment / tracking_area;	//Calculate the vertical position of the object
+			 horizontal_last = posX;	//Getting a new horizontal position
+			 vertical_last = posY;	//Getting a new vertical position value
 	      	}
-	      	frameRGB = frameRGB + track_motion;//drawing continuous line in original video frames//
-            	g_print("Position of the object is: %d, %d \n", Horizontal_Last, vertical_Last);//showing tracked co-ordinated values//
+            	g_print("Position of the object is: %d, %d \n", horizontal_last, vertical_last);	//Showing tracked co-ordinated values
 	      
-	      	// Write location data to file
+	      	/* Write location data to files */
 	      	fstream file;
 	      	
  		file.open("PosX.txt", ios::out | ios::app);
-		//taking input from frame to write to PosX.txt file
-		string message = to_string(Horizontal_Last);
+		string message = to_string(horizontal_last); //Taking input from each frame to write to PosX.txt file
 		file<< message << "," << endl;
 		file.close();
 		
 		file.open("PosY.txt", ios::out | ios::app);
-		//taking input from frame to write to PosY.txt file
-		message = to_string(vertical_Last);
+		message = to_string(vertical_last); //Taking input from each frame to write to PosY.txt file
 		file<< message << "," << endl;
 		file.close();
 	      
@@ -140,11 +138,11 @@ int main (int   argc, char *argv[])
   loop = g_main_loop_new (NULL, FALSE);
 
   /* Create gstreamer elements */
-  element.pipeline =          gst_pipeline_new ("video-player");
-  element.source   =          gst_element_factory_make ("v4l2src",       "cam-source");
-  element.filter   = 	       gst_element_factory_make ("capsfilter", "video-filter");
-  element.decoder  =          gst_element_factory_make ("jpegdec",     "jpeg-decoder");
-  element.sink     =          gst_element_factory_make ("appsink", "appsink");
+  element.pipeline	=	gst_pipeline_new ("video-player");
+  element.source	=	gst_element_factory_make ("v4l2src",       "cam-source");
+  element.filter	=	gst_element_factory_make ("capsfilter", "video-filter");
+  element.decoder	=	gst_element_factory_make ("jpegdec",     "jpeg-decoder");
+  element.sink	=	gst_element_factory_make ("appsink", "appsink");
 
   if (!element.pipeline || !element.source || !element.filter || !element.decoder || !element.sink) {
     g_printerr ("One element could not be created. Exiting.\n");
@@ -155,7 +153,7 @@ int main (int   argc, char *argv[])
 
   /* we set the input filename to the source element */
   g_object_set (G_OBJECT (element.source), "device", "/dev/video0", NULL);
-  caps = gst_caps_new_simple("image/jpeg", "format", G_TYPE_STRING, "RGB", "framerate", GST_TYPE_FRACTION, 30, 1, "width", G_TYPE_INT, 320, "height", G_TYPE_INT, 240, NULL);
+  caps = gst_caps_new_simple("image/jpeg", "format", G_TYPE_STRING, "RGB", "framerate", GST_TYPE_FRACTION, 30, 1, "width", G_TYPE_INT, res_w, "height", G_TYPE_INT, res_h, NULL);
   g_object_set(G_OBJECT(element.filter), "caps", caps, NULL);
 
   /* we add a message handler */
